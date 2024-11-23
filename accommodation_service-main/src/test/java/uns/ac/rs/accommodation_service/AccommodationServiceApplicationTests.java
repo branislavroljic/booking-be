5efe26fd30aa45490a69cc8828c7039e79b6392c
Branislav Roljic <branislavroljic99@gmail.com>
2024-11-23 20:44:45 +0100

    feat: initial commit

---
+++ accommodation_service-main/src/test/java/uns/ac/rs/accommodation_service/AccommodationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uns.ac.rs.accommodation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccommodationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
